57143c0f246fac01329003172ab356cb2a10ec4f
Alausa Aishat <qtaisha.ao@gmail.com>
2022-08-30 19:28:15 +0100

    lab completed

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
   # write code here
-end
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+
+  def kind
+    if isvalid_triangle?(side1, side2, side3)
+      if side1 == side2 && side2 == side3
+        return :equilateral
+      elsif 
+        side1 == side2 || side1 == side3 || side2 == side3
+        return :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+    class TriangleError < StandardError
+      def message
+        "Invalid Triangle"
+      end
+    end
+
+    private
+    
+    def isvalid_triangle? (len1, len2, len3)
+      (len1 && len2 && len3 > 0) && (len1 + len2 > len3) && (len1 + len3 > len2) && (len2 + len3 > len1)
+    end
+  end
